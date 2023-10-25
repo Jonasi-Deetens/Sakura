29815f1a06726c67aec1b8a72198968e26ce1c80
Jonasi-Deetens <jonasideetens@hotmail.com>
2023-10-25 10:24:24 +0200

    Add hamburger menu to all nav with a script, and a quote to homepage

---
+++ scripts/hamburgerNav.js
@@ -0,0 +1,11 @@
+function openNav() {
+    var nav = document.getElementsByTagName("nav")[0];
+    var ul = nav.getElementsByTagName("ul")[0];
+
+    if (ul.classList.contains("hidden")) {
+        ul.classList.remove("hidden");
+    } else if (!ul.classList.contains("hidden")) {
+        ul.classList.add("hidden")
+    }
+
+}
