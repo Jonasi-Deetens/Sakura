706993d8e928dfbb7448dcf1f8175478f96fb95d
Jonasi-Deetens <jonasideetens@hotmail.com>
2023-10-25 11:50:22 +0200

    Add media with dpi to scale main on 200% zoom, and fix scripts errors

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,10 @@
-setInterval(generateLeaves, 1000);
+initScripts();
+
+function initScripts() {
+    console.log(document.getElementsByClassName("falling-leaves-container").length);
+    if (document.getElementsByClassName("falling-leaves-container").length != 0){ setInterval(generateLeaves, 1000); }
+    if (document.getElementsByClassName("floating-leaves-container").length != 0){ setInterval(generateFloatingLeaves, 1000); }
+}
 
 function generateLeaves() {
     var image = new Image(4,4);
@@ -22,6 +28,36 @@ function generateLeaves() {
       ],
       {
         duration: 5000, // 5 seconds for the leaf to fall
+        easing: "linear",
+      }
+    );
+  
+    // Remove the image element after the animation is complete
+    animation.onfinish = function () {
+      image.remove();
+    };
+
+}
+
+function generateFloatingLeaves() {
+    var image = new Image(4,4);
+    image.src = "../../images/sakura-tree-leaf-" + Math.round(Math.random()* 5 + 1) + ".png";
+    image.alt = "Sakura leaf floating";
+    image.style.position = "absolute";
+    image.style.left = "-40px"; // Start left of the screen
+    image.style.top = Math.random() * 10 + "%"; // Random vertical position
+    image.classList.add("smallImg");
+    image.classList.add("rounded-full");
+    document.getElementsByClassName("floating-leaves-container")[0].appendChild(image);
+  
+    // Animate the falling leaf
+    var animation = image.animate(
+      [
+        { left: "-40px" },
+        { left: "100%" }, // Fall to the bottom of the viewport
+      ],
+      {
+        duration: 10000, // 5 seconds for the leaf to fall
         easing: "linear",
       }
     );
